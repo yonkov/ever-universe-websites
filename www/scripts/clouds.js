6ac5a5828c948983106914de36b9abed38fc590d
Dimana <dimsi123.dc@gmail.com>
2019-12-11 19:16:33 +0200

    Info-cloud animation delay fixed

--- www/scripts/clouds.js
+++ www/scripts/clouds.js
@@ -6,17 +6,13 @@ clouds.forEach(cloud=>{
         e.stopPropagation();
         let descriptionDiv =[...this.children][1].children;
         let close = descriptionDiv[0];
-        let hiddenP = descriptionDiv[2];
         close.addEventListener("click",(e)=>{
             e.preventDefault();
             e.stopPropagation();
             this.setAttribute("class", "cloud")
-            hiddenP.setAttribute("style","display:none;");
         
         })
         this.setAttribute("class","info-cloud");
-        window.setTimeout(() => {
-            hiddenP.setAttribute("style","display:block;");
-        }, 250);
+       
     })
 })
